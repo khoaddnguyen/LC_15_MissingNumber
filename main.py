3e74b21f37403e16239c96dbdacb7781e8fff254
kwa_laa <khoaddnguyen@gmail.com>
2023-10-27 16:52:15 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# given array with n numbers in range [0, n]
+# return the missing number
+def missingNumber(self, nums: List[int]) -> int:
+    result = len(nums)
+
+    for i in range(len(nums)):
+        result += (i - nums[i])
+
+    return result
