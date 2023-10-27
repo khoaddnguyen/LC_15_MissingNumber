2b432ed64839843acf48aa612f3995361d4d35d5
kwa_laa <khoaddnguyen@gmail.com>
2023-10-27 16:55:39 -0500

    update code

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def missingNumber(self, nums: List[int]) -> int:
     result = len(nums)
 
     for i in range(len(nums)):
-        result += (i - nums[i])
+        result += i - nums[i]
 
     return result
